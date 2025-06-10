0f5106bc15851d8ab18fde7b7872b2a85fd85cc5
Paul Jeschke <pjeschke@hmp-solutions.online>
2025-06-10 13:07:16 +0200

    remove statice webasset base path and define a basepath in Program.cs

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -4,12 +4,13 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Configuration.SetBasePath("/blazorapp1/");
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddFluentUIComponents();
 builder.Services.AddDataGridODataAdapter();
 builder.Services.AddHttpClient();
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+var app = builder.Build();
+await app.RunAsync();
